96734fd3d92aa91d166b5e802208456a881fb15a
aflyingmole <167077766+aflyingmole@users.noreply.github.com>
2024-05-23 11:54:43 +0900

    gd

---
+++ src/connectionEx01/ConnectionEx09.java
@@ -0,0 +1,51 @@
+package connectionEx01;
+
+import java.sql.*;
+
+public class ConnectionEx09 {
+
+    public static void main(String[] args) {
+        System.out.println("시작");
+
+        String url = "jdbc:mariadb://localhost:3306/sample";
+        String user = "root";
+        String password = "123456";
+
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            System.out.println("드라이버 로딩 성공");
+
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("데이터베이스 연결 성공") ;
+
+            stmt = conn.createStatement();
+
+            String sql = String.format("select * from dept2");
+            rs = stmt.executeQuery(sql);
+
+            // 끝까지 읽기
+            while(rs.next()) {
+                System.out.printf("%s\t%s\t%s%n",rs.getString("deptno"),rs.getString("dname"),rs.getString("loc"));
+            }
+
+            // 한줄 읽기
+            if(rs.next()){
+                System.out.printf("%s\t%s\t%s%n",rs.getString("deptno"),rs.getString("dname"),rs.getString("loc"));
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("[에러]" + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("[에러]" + e.getMessage());
+        } finally {
+            if(rs != null)try{rs.close();}catch(SQLException e){}
+            if(stmt != null)try{stmt.close();}catch(SQLException e){}
+            if(conn != null)try{conn.close();}catch(SQLException e){}
+        }
+        System.out.println("끝");
+    }
+}
---
+++ src/connectionEx01/ConnectionTest.java
@@ -0,0 +1,49 @@
+package connectionEx01;
+
+import java.sql.*;
+
+public class ConnectionTest {
+    public static void main(String[] args) {
+        System.out.println("시작");
+
+        String url = "jdbc:mariadb://localhost:3306/sample";
+        String user = "root";
+        String password = "123456";
+
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            System.out.println("드라이버 로딩 성공");
+
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("데이터베이스 연결 성공") ;
+
+            stmt = conn.createStatement();
+
+            String sql = String.format("SELECT empno, ename, sal, sal * 12 + ifnull(comm,0) AS annsal FROM emp WHERE deptno = 10");
+            rs = stmt.executeQuery(sql);
+
+            while(rs.next()) {
+                int empno = rs.getInt("empno");
+                String ename = rs.getString("ename");
+                int sal = rs.getInt("sal");
+                int annsal = rs.getInt("annsal");
+
+                System.out.printf("사원번호: %d, 사원이름: %s, 급여: %d, annsal: %d%n", empno, ename, sal, annsal);
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("[에러]" + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("[에러]" + e.getMessage());
+        } finally {
+            if(rs != null)try{rs.close();}catch(SQLException e){}
+            if(stmt != null)try{stmt.close();}catch(SQLException e){}
+            if(conn != null)try{conn.close();}catch(SQLException e){}
+        }
+        System.out.println("끝");
+    }
+}
