3cdcf82b7422eda4958a6c223793fc78cdc0f0bf
aflyingmole <167077766+aflyingmole@users.noreply.github.com>
2024-05-23 10:50:35 +0900

    Initial commit

---
+++ src/connectionEx01/ConnectionEx01.java
@@ -0,0 +1,18 @@
+package connectionEx01;
+
+public class ConnectionEx01 {
+
+    public static void main(String[] args) {
+        System.out.println("시작");
+
+        try {
+            // 데이터베이스 연결을 위한 데이터베이스 드라이버 로딩
+            Class.forName("org.mariadb.jdbc.Driver");
+            System.out.println("드라이버 로딩 성공");
+        } catch (ClassNotFoundException e) {
+            System.out.println("[에러] 드라이버 로딩 실패");
+            e.printStackTrace();
+        }
+        System.out.println("끝");
+    }
+}
---
+++ src/connectionEx01/ConnectionEx03.java
@@ -0,0 +1,33 @@
+package connectionEx01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionEx03 {
+
+    public static void main(String[] args) {
+        System.out.println("시작");
+
+        String url = "jdbc:mariadb://localhost:3306/sample";
+        String user = "root";
+        String password = "123456";
+
+        Connection conn = null;
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            System.out.println("드라이버 로딩 성공");
+
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("데이터베이스 연결 성공") ;
+        } catch (ClassNotFoundException e) {
+            System.out.println("[에러]" + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("[에러]" + e.getMessage());
+        } finally {
+            if(conn != null)try{conn.close();}catch(SQLException e){}
+        }
+        System.out.println("끝");
+    }
+}
