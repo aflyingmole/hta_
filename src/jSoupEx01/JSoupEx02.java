e8fe8973830d9a3113cf107f406dace552dbd224
aflyingmole <167077766+aflyingmole@users.noreply.github.com>
2024-06-03 16:58:21 +0900

    gd

--- src/jSoupEx01/JSoupEx02.java
+++ src/jSoupEx01/JSoupEx02.java
@@ -7,30 +7,31 @@ import org.jsoup.select.Elements;
 
 public class JSoupEx02 {
     public static void main(String[] args) {
+        String strHtML = "<html>" +
+                "<head>" +
+                "<title> Hello JSoup</title>" +
+                "</head>" +
+                "<body>" +
+                "<p id = 'i1' class = 'c1'> hello doc 1 </p>" +
+                "<p id = 'i2' class = 'c2'> hello doc 2 </p>" +
+                "<p id = 'i3' class = 'c1'> hello doc 3 </p>" +
+                "<p id = 'i4' class = 'c2'> hello doc 4 </p>" +
+                "<p>hello jsoup doc1" +
+                "<p>hello jsoup doc1" +
+                "</body>" +
+                "</html>";
 
-        String strHtml = "<html>"
-                + "<head>"
-                + "<title>Hello JSoup</title>"
-                + "</head>"
-                + "<body>"
-                + "<p id='i1' class='c1'>hello JSoup document 1<p>"
-                + "<p id='i2' class='c2'>hello JSoup document 2<p>"
-                + "<p id='i3' class='c1'>hello JSoup document 3<p>"
-                + "<p id='i4' class='c2'>hello JSoup document 4<p>"
-                + "</body>"
-                + "</html>";
-
-        Document doc = Jsoup.parse(strHtml);
+        Document doc = Jsoup.parse(strHtML);
 
         Element pTag = doc.getElementById("i1");
         System.out.println(pTag);
         System.out.println(pTag.text());
-
         Elements pTags = doc.getElementsByClass("c1");
         System.out.println(pTags);
 
         for (Element tag : pTags) {
             System.out.println(tag.text());
         }
+
     }
 }
