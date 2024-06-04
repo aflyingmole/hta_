db19000716e92a6146e938a900c5fb6cfb5d3644
aflyingmole <167077766+aflyingmole@users.noreply.github.com>
2024-06-04 14:52:04 +0900

    ㅎㅇ

--- src/netWorkEx02/JSONEx01.java
+++ src/netWorkEx02/JSONEx01.java
@@ -1,7 +1,33 @@
 package netWorkEx02;
 
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class JSONEx01 {
     public static void main(String[] args) {
 
+
+        // json - simple 연습
+        // validation 검사를 반드시 할 것
+        //String strJson = "[ 1,2,3,4 ]";
+        String strJson = "[ \"1\", \"2\", \"3\", \"4\" ]";
+
+
+        JSONParser parser = new JSONParser();
+
+        try {
+            JSONArray arr = (JSONArray) parser.parse(strJson);
+            System.out.println(arr);
+
+            for (int i = 0; i < arr.size(); i++) {
+
+                // 정수 => Long
+                String data = (String) arr.get(i);
+                System.out.println(data);
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/netWorkEx02/JSONEx02.java
@@ -0,0 +1,28 @@
+package netWorkEx02;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class JSONEx02 {
+    public static void main(String[] args) {
+
+
+        String strJson = "{ \"key1\":\"value1\", \"key2\":\"value2\" }";
+
+        JSONParser parser = new JSONParser();
+
+        try {
+            JSONObject obj = (JSONObject) parser.parse(strJson);
+            System.out.println(obj.toString());
+
+            // Map 구조
+            String key1 = (String) obj.get("key1");
+            System.out.println(key1);
+
+        } catch (ParseException e) {
+            System.out.println("[에러]" + e.getMessage());
+        }
+
+    }
+}
