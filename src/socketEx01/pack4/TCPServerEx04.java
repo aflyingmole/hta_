7f514ff888eed78e69f061e01f2eaceb51c381f0
aflyingmole <167077766+aflyingmole@users.noreply.github.com>
2024-06-05 14:24:52 +0900

    ㅎㅇ

---
+++ src/socketEx01/pack4/TCPServerEx04.java
@@ -0,0 +1,52 @@
+package socketEx01.pack4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class TCPServerEx04{
+
+    public static void main(String[] args) {
+
+        Socket socket = null;
+
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+
+        try {
+            System.out.println("클라이언트와 연결 합니다.");
+
+            socket = new Socket("localhost",7777);
+            System.out.println("클라이언트와 연결 되었습니다.");
+
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream(),"utf-8"));
+            bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(),"utf-8"));
+
+            String msg = br.readLine();
+            System.out.println("클라이언트에서 보낸 메시지 : " + msg);
+
+            bw.write(msg + System.lineSeparator());
+            bw.flush(); //스트림 비우는 문법
+
+            System.out.println("전송 완료");
+
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            System.out.println("[에러]"+e.getMessage());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            System.out.println("[에러]"+e.getMessage());
+        }finally {
+            if(br != null)try {br.close();}catch(IOException e) {}
+            if(bw != null)try {bw.close();}catch(IOException e) {}
+
+            if(socket != null)try {socket.close();}catch(IOException e) {}
+        }
+
+    }
+
+}
---
+++ src/socketEx01/pack5/TCPServerEx05.java
@@ -0,0 +1,42 @@
+package socketEx01.pack5;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServerEx05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+		
+		try {
+			serverSocket = new ServerSocket( 7777 );
+			
+			// 항상 처리 후 대기 상태
+			while( true ) {
+				try {
+					System.out.println( "서버가 준비되었습니다" );
+				
+					socket = serverSocket.accept();
+					System.out.println( "클라이언트가 연결되었습니다." );
+					
+				} catch( IOException e ) {
+					// TODO Auto-generated catch block
+					System.out.println( "[에러] " + e.getMessage() );	
+				} finally {
+					if( socket != null ) try { socket.close(); } catch( IOException e ) {}	
+				}					
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println( "[에러] " + e.getMessage() );
+		} finally {
+			if( serverSocket != null ) try { serverSocket.close(); } catch( IOException e ) {}
+		}
+	}
+
+}
